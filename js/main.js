80d42c24499cf0ff3ae9feae045f9c41103addb1
Michael Markoski <michael@exportsmedia.com>
2019-01-03 20:30:00 -0600

    work on header

--- js/main.js
+++ js/main.js
@@ -5,3 +5,64 @@ jQuery(window).scroll(function ($) {
         jQuery('header').removeClass("sticky");
     }
 });
+
+
+jQuery(document).ready(function () {
+
+    var getMax = function () {
+        return jQuery('.container .content').height() - jQuery(window).height();
+    }
+
+    var getValue = function () {
+        return jQuery(window).scrollTop();
+    }
+
+    if ('max' in document.createElement('progress')) {
+        var progressBar = jQuery('progress');
+
+        progressBar.attr({
+            max: getMax()
+        });
+
+        jQuery(document).on('scroll', function () {
+            progressBar.attr({
+                value: getValue()
+            });
+        });
+
+        jQuery(window).resize(function () {
+
+            progressBar.attr({
+                max: getMax(),
+                value: getValue()
+            });
+        });
+
+    } else {
+
+        var progressBar = jQuery('.progress-bar'),
+            max = getMax(),
+            value, width;
+
+        var getWidth = function () {
+
+            value = getValue();
+            width = (value / max) * 100;
+            width = width + '%';
+            return width;
+        }
+
+        var setWidth = function () {
+            progressBar.css({
+                width: getWidth()
+            });
+        }
+
+        jQuery(document).on('scroll', setWidth);
+        jQuery(window).on('resize', function () {
+
+            max = getMax();
+            setWidth();
+        });
+    }
+});
--- menu.php
+++ menu.php
@@ -3,27 +3,43 @@
  * @package WordPress
  * @subpackage WP-Mike-Notes
  */
+
+global $post;
 ?>
   <header class="site-header" itemscope="" itemtype="https://schema.org/WPHeader">  
 
     <div class="row">
     
-        <div class="col-6"> 
+        <div class="col-12"> 
             <div class="logo">
-                <a href="<?php echo home_url(); ?>" class="text-white h2" style="text-decoration: none;vertical-align: middle;">
+                <a href="<?php echo home_url(); ?>" class="h2" style="margin:0; text-decoration: none;">
                     <span class="font-weight-bold">Michael's</span><span class="font-weight-light">Minutes</span>
                 </a>
-                <span class="description h6 text-light ml-4"><?php echo get_bloginfo('description');?></span>
+                <span class="description h6 ml-4" style="vertical-align: middle;"><?php echo get_bloginfo('description');?></span>
+                <?php if(isset($post)) { ?>
+                    <span class="page-title h6 ml-4" style="vertical-align: middle;"><?php echo $post->post_title;?></span>
+                <?php } ?>
             </div>
-        </div> 
-        
-        <div class="col-6">
 
             <!--  the Menu -->
-            <?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_id' => 'main-menu', ) ); ?>
+            <?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_id' => 'main-menu', 'container_class' => 'float-right' ) ); ?>
 
         </div>
 
     </div>
+
+    <?php if(is_single()) { ?>
+
+        <progress value="0" max="1">
+            <!-- Older browsers look to the old div style of progress bars -->
+            <!-- Newer browsers ignore this stuff but it allows older browsers to 
+                    still have a progress bar. -->
+            <div class="progress-container">
+                <span class="progress-bar"></span>    
+            </div>
+        </progress>
+
+    <?php } ?>
+
 </header> <!--  End blog header -->
    
